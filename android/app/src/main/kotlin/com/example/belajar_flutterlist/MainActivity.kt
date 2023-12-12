e3e47ece34f9712dfbe14b992e80705b65fa2253
prasetyoediwibowo <prasetyoedi943@gmail.com>
2023-12-12 11:55:46 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/belajar_flutterlist/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.coba_flutterlist
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:coba_flutterlist/userdata.dart';
+import 'package:coba_flutterlist/useritem.dart';
+import 'package:coba_flutterlist/userlist.dart';
+
+void main() {
+  // UserData userIdris = UserData("Idris", 34, "idrez.mochamad@gmail.com");
+  runApp(UserList());
+}
